994d84f0c06c72f9f6cc090db74904d605ee9d13
SYSTEM <SYSTEM@DESKTOP-UHFT4RI>
2022-09-03 01:55:21 -0300

    Update

--- bytebank/src/bytebank/Conta.java
+++ bytebank/src/bytebank/Conta.java
@@ -1,20 +1,24 @@
 package bytebank;
 
 public class Conta {
-	
+
 	double saldo;
 	int agencia;
 	int numero;
 	String titular;
-	
-	
-	public void deposita (double valor) {
+
+	public void deposita(double valor) {
 		this.saldo = saldo + valor;
 	}
 
-	public void saca (double valor) {
-		this.saldo = saldo - valor;
+	public void saca(double valor) {
+		if (saldo > valor) {
+			this.saldo = saldo - valor;
+			System.out.println("Operação Realizada Com Sucesso!");
+		} else {
+			System.out.println("Valor Indisponível: " + valor + " \n O Seu Saldo Atual é :" + saldo);
+		}
+
 	}
-	
-	
+
 }
--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -1,19 +1,16 @@
 package bytebank;
 
 public class TestaMetodo {
-	
+
 	public static void main(String[] args) {
-		
+
 		Conta contaCliente1 = new Conta();
 		contaCliente1.saldo = 100;
 		contaCliente1.deposita(150);
-		contaCliente1.saca(50);
-		
-		
+
 		System.out.println(contaCliente1.saldo);
-		
-		
-		
+
+		contaCliente1.saca(350);
 	}
 
 }
