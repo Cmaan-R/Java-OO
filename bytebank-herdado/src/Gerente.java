079b608007d95244bd9cf26e54fa579b682d2c58
SYSTEM <SYSTEM@192.168.200.110>
2022-09-08 10:56:30 -0300

    Update

---
+++ bytebank-herdado/src/Gerente.java
@@ -0,0 +1,18 @@
+//Gerente Herda da Classe Funcionário
+public class Gerente extends Funcionario{
+
+	private int senha;
+
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+//	public double getBonificacao() {
+//		return this.salario;
+//	}
+
+}
