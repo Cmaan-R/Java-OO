fcd2f287dbf209363906e6b6fb384e9c2d0f317a
SYSTEM <SYSTEM@192.168.200.110>
2022-09-08 10:31:22 -0300

    Update

--- bytebank-herdado/src/Funcionario.java
+++ bytebank-herdado/src/Funcionario.java
@@ -1,29 +1,40 @@
 
 public class Funcionario {
-	
+
 	private String nome;
 	private String cpf;
 	private Double salario;
+
+	public Funcionario() {
+
+	}
+
+	public double getBonificacao() {
+		return this.salario * 0.1;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public Double getSalario() {
 		return salario;
 	}
+
 	public void setSalario(Double salario) {
 		this.salario = salario;
 	}
-	
-	
-	
 
 }
---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,16 @@
+
+public class TesteFuncionario {
+
+	public static void main(String[] args) {
+
+		Funcionario f1 = new Funcionario();
+
+		f1.setNome("Nico Steppat");
+		f1.setCpf("22335566 - 9");
+		f1.setSalario(2600.00);
+
+		System.out.println(f1.getNome() + " " + f1.getBonificacao());
+
+	}
+
+}
