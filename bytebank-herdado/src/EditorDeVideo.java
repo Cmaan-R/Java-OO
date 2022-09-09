2a343ff711f67d595b16514311298059ea3aed0b
SYSTEM <SYSTEM@DESKTOP-UHFT4RI>
2022-09-09 13:11:16 -0300

    Update

--- bytebank-herdado/src/EditorDeVideo.java
+++ bytebank-herdado/src/EditorDeVideo.java
@@ -2,6 +2,7 @@
 public class EditorDeVideo extends Funcionario{
 
 	public double getBonificacao() {
+		System.out.println("Chamando o Método de Bonificação do Editor De Vídeo");
 		return super.getBonificacao() + 100;
 	}
 	
