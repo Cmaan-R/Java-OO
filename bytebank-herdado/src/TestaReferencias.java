07f20f02347ea3ded8b21241682e11a099aa03ff
SYSTEM <SYSTEM@DESKTOP-UHFT4RI>
2022-09-09 13:03:53 -0300

    Update

--- bytebank-herdado/src/TestaReferencias.java
+++ bytebank-herdado/src/TestaReferencias.java
@@ -3,12 +3,23 @@ public class TestaReferencias {
 
 	public static void main(String[] args) {
 
-		Funcionario g1 = new Gerente();
-
+		Gerente g1 = new Gerente();
 		g1.setNome("Marcos");
-		String nome = g1.getNome();
+		g1.setSalario(5000.0);
+		
+		Funcionario f1 = new Funcionario();
+		f1.setSalario(2000.0);
+		
+		EditorDeVideo e1 = new EditorDeVideo();
+		e1.setSalario(2500.0);
+		
+		ControleBonificacao controle = new ControleBonificacao();
+		controle.registra(g1);
+		controle.registra(f1);
+		controle.registra(e1);
 
-		System.out.println(nome);
+		
+		System.out.println(controle.getSoma());
 
 	}
 }
