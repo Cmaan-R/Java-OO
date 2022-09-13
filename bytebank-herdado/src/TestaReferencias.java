f36b1afa2cd09bd323039e27a0c2d2a67cd9993d
SYSTEM <SYSTEM@192.168.200.110>
2022-09-13 11:41:12 -0300

    Update

--- bytebank-herdado/src/TestaReferencias.java
+++ bytebank-herdado/src/TestaReferencias.java
@@ -7,9 +7,6 @@ public class TestaReferencias {
 		g1.setNome("Marcos");
 		g1.setSalario(5000.0);
 
-		Funcionario f1 = new Funcionario();
-		f1.setSalario(2000.0);
-
 		EditorDeVideo e1 = new EditorDeVideo();
 		e1.setSalario(2500.0);
 
@@ -18,7 +15,6 @@ public class TestaReferencias {
 
 		ControleBonificacao controle = new ControleBonificacao();
 		controle.registra(g1);
-		controle.registra(f1);
 		controle.registra(e1);
 		controle.registra(d1);
 
