d29c3feb43563cc2a2739f65ba1a13b350391783
SYSTEM <SYSTEM@192.168.200.110>
2022-09-13 12:32:16 -0300

    Update

---
+++ bytebank-herdado/src/SistemaInterno.java
@@ -0,0 +1,15 @@
+
+public class SistemaInterno {
+	
+	private int senha = 222;
+	
+	public void autentica(Gerente g) {
+		boolean autenticou = g.autentica(this.senha);
+		if(autenticou) {
+			System.out.println("Acesso Concedido");
+		} else {
+			System.out.println("Acesso Negado");
+		}
+	}
+
+}
