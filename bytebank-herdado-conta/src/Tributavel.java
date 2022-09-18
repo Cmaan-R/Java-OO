3eca69c6281451e07a77e2edd7e60129a24c7f4b
Windows 10 <Windows 10@DESKTOP-ANGJ5V6>
2022-09-18 16:12:11 -0300

    Update

---
+++ bytebank-herdado-conta/src/Tributavel.java
@@ -0,0 +1,7 @@
+//Interface == Contrato //Interface sempre é abstract
+public interface Tributavel {
+
+	// public abstract abaixo, mesmo sem ser declarado explicidamente
+	double getValorImposto();
+
+}
