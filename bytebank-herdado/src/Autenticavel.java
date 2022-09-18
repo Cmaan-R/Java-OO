fd81fcc1dae886f2d19a2a86b747f987ba11a4bc
Windows 10 <Windows 10@DESKTOP-ANGJ5V6>
2022-09-18 15:44:32 -0300

    Update

--- bytebank-herdado/src/Autenticavel.java
+++ bytebank-herdado/src/Autenticavel.java
@@ -3,9 +3,14 @@
 		//método setSenha
 			//método Autentica
 public abstract interface Autenticavel {
+	
+	
 
 	public abstract void setSenha(int senha);
 
 	public abstract boolean autentica(int senha);
+	
+	
+	
 
 }
