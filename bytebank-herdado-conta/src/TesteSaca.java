48fd4cfc2b42fcc1e921931f5a688cb240384f20
Windows 10 <Windows 10@DESKTOP-ANGJ5V6>
2022-09-25 14:49:32 -0300

    Update

--- bytebank-herdado-conta/src/TesteSaca.java
+++ bytebank-herdado-conta/src/TesteSaca.java
@@ -3,13 +3,18 @@ public class TesteSaca {
 
 	public static void main(String[] args) {
 
-		Conta conta = new ContaCorrente(123 , 231);
+		Conta conta = new ContaCorrente(123, 231);
+
 		conta.deposita(200.0);
-		conta.saca(210.0);
 		
+		try {
+			conta.saca(210.0);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
 		System.out.println(conta.saldo);
-			
-		
+
 	}
 
 }
