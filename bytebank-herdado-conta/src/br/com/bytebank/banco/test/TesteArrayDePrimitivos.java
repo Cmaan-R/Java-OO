ff5cec26c430e69000a37c209d01a76a87beed95
Windows 10 <Windows 10@192.168.200.171>
2022-09-27 14:46:58 -0300

    Update

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteArrayDePrimitivos.java
@@ -0,0 +1,20 @@
+package br.com.bytebank.banco.test;
+
+public class TesteArrayDePrimitivos {
+
+	public static void main(String[] args) {
+
+		int[] idades = new int[5]; // Inicializa o Array Com os Padrões 0
+
+		for (int i = 0; i < idades.length; i++) {
+
+			idades[i] = i * i;
+
+		}
+
+		for ( int i = 0 ; i < idades.length; i++) {
+			System.out.println(idades[i]);
+		}
+	}
+
+}
