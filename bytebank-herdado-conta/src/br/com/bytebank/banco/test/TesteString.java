bd4df05e3812be77856dc9fb5dbdd2d10fd4e191
Windows 10 <Windows 10@192.168.200.171>
2022-09-27 11:55:17 -0300

    Update

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteString.java
@@ -0,0 +1,24 @@
+package br.com.bytebank.banco.test;
+
+public class TesteString {
+
+	public static void main(String[] args) {
+
+		String nome = "Alura"; // Object Literal
+//		String outro = new String("Alura");
+		
+//		char c = nome.charAt(2);
+//		System.out.println(c);
+
+//		char c ='A';
+//		char d ='a';
+
+//		String outra = nome.replace(c, d);
+
+//		System.out.println(nome);
+		
+
+
+	}
+
+}
