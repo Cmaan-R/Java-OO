01f6077e6677fc031698305a2a800bbaca01aeda
Windows 10 <Windows 10@192.168.200.171>
2022-09-27 15:48:16 -0300

    Update

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/GuadadorDeContas.java
@@ -0,0 +1,24 @@
+package br.com.bytebank.banco.test;
+
+import br.com.bytebank.banco.modelo.Conta;
+
+public class GuadadorDeContas {
+
+	private Conta[] referencias;
+	private int posicaoLivre;
+
+	public GuadadorDeContas() {
+		this.referencias = new Conta[10];
+		this.posicaoLivre = 0;
+	}
+
+	public void adiciona(Conta ref) {
+		this.referencias[this.posicaoLivre] = ref;
+		posicaoLivre++;
+	}
+
+	public int getQuantidaDeElementos() {
+		return this.posicaoLivre;
+	}
+
+}
---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
@@ -0,0 +1,23 @@
+package br.com.bytebank.banco.test;
+
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+
+public class Teste {
+	
+	public static void main(String[] args) {
+		
+		GuadadorDeContas guadador = new GuadadorDeContas();
+		
+		Conta cc = new ContaCorrente(22, 11);
+		guadador.adiciona(cc);
+		
+		Conta cc2 = new ContaCorrente(22, 22);
+		guadador.adiciona(cc2);
+		
+		int tamanho = guadador.getQuantidaDeElementos();
+		System.out.println(tamanho);
+		
+	}
+
+}
