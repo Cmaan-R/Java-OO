14a9f2029ab1260e22e39e9feda4356485588a37
Windows 10 <Windows 10@192.168.200.171>
2022-09-27 14:01:33 -0300

    Update

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/Teste.java
@@ -1,38 +1,23 @@
 package br.com.bytebank.banco.test;
 
-import br.com.bytebank.banco.modelo.ContaCorrente;
-import br.com.bytebank.banco.modelo.ContaPoupanca;
-
 public class Teste {
-
+	
 	public static void main(String[] args) {
+		
+		int[] idades = new int[5]; //Inicializa o Array Com os Padrões 0
+		
+		int idade1 = idades[0];
 
-//		System.out.println("x");
-//		System.out.println(3);
-//		System.out.println(false);
-
-		Object cc = new ContaCorrente(22, 33);
-		Object cp = new ContaPoupanca(33, 22);
-//		Object c = new Cliente();
-
-		System.out.println(cc);
-		System.out.println(cp);
-
-//		println(c);
-
-	}
-
-	static void println() {
-	}
-
-	static void println(int a) {
-	}
-
-	static void println(boolean valor) {
-	}
-
-	static void println(Object referencia) {
-
+		System.out.println(idade1);
+		
+		
+		
+		
+	
+		
+		
+		
+		
 	}
 
 }
