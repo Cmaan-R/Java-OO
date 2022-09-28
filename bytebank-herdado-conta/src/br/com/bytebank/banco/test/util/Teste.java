8cb47e84025f77b3ec04b98ab1c1a84433629d81
Windows 10 <Windows 10@192.168.200.171>
2022-09-28 08:30:10 -0300

    Update

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/util/Teste.java
@@ -2,7 +2,6 @@ package br.com.bytebank.banco.test.util;
 
 import java.util.ArrayList;
 
-import br.com.bytebank.banco.modelo.Cliente;
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 
@@ -10,42 +9,41 @@ public class Teste {
 
 	public static void main(String[] args) {
 
-		ArrayList lista = new ArrayList();
+		ArrayList<Conta> lista = new ArrayList<Conta>();
 
-//		Conta cc = new ContaCorrente(22, 11);
-		Cliente cliente = new Cliente();
-		lista.add(cliente);
+		Conta cc = new ContaCorrente(22, 11);
+		lista.add(cc);
 
 		Conta cc2 = new ContaCorrente(22, 22);
 		lista.add(cc2);
 
 		System.out.println("Tamanho: " + lista.size());
 
-		Conta ref = (Conta) lista.get(0);
+		Conta ref = lista.get(0);
 
 		System.out.println(ref.getNumero());
 
 		lista.remove(0);
-		
+
 		System.out.println("Tamanho: " + lista.size());
-		
+
 		Conta cc3 = new ContaCorrente(33, 311);
 		lista.add(cc3);
 
 		Conta cc4 = new ContaCorrente(33, 322);
 		lista.add(cc2);
-		
-		for(int i = 0; i < lista.size(); i++) {
+
+		for (int i = 0; i < lista.size(); i++) {
 			Object oRef = lista.get(i);
 			System.out.println(oRef);
 		}
-		
+
 		System.out.println("--------------------------------------");
-		
-		for(Object oRef: lista) {
-			System.out.println(oRef);
+
+		for (Conta conta : lista) {
+			System.out.println(conta);
 		}
-		
+
 	}
 
 }
